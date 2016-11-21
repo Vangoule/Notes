8ab2f00128789ceb5b288dacc7a37ddb2732dcea
vangoule <vangoule273@gmail.com>
2016-11-21 21:16:14 +0000

    Added tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,6 +4,9 @@ class HomeControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select 'title', 'Notes'
+    assert_select 'h1', 'Notes'
+    assert_select 'p', 'Feel free to write a note!'
   end
 
 end
